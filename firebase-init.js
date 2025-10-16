16c7e28170a63b63ccbac7e790e019c918209be4
hari h jadhav <jadhav.h@gmail.com>
2025-10-16 23:25:52 +0530

    Add Firebase configuration script

---
+++ firebase-init.js
@@ -0,0 +1,10 @@
+<script>
+window.firebaseConfig = {
+  apiKey: "AIzaSyCHAYkKUEsGateO9VkIbOm16dFezVVz2Q",
+  authDomain: "kasturidivine-in.firebaseapp.com",
+  projectId: "kasturidivine-in",
+  storageBucket: "kasturidivine-in.firebasestorage.app",
+  messagingSenderId: "637084935513",
+  appId: "1:637084935513:web:bedbd411d6a8455f4adc48"
+};
+</script>
